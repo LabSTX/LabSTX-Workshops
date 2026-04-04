961941dee18cd717676e769568621669c5f05ecb
ProdevappOFFICIAL <122856789+ProdevappOFFICIAL@users.noreply.github.com>
2026-04-04 23:27:10 +0100

    Added Stxer.xyz simulation demo project to templates

--- fungible-token/contracts/my-token.clar
+++ fungible-token/contracts/my-token.clar
@@ -6,29 +6,46 @@
 (define-data-var token-uri (optional (string-utf8 256)) none)
 
 ;; Public functions
-(define-public (transfer (amount uint) (sender principal) (recipient principal) (memo (optional (buff 34))))
+(define-public (transfer
+    (amount uint)
+    (sender principal)
+    (recipient principal)
+    (memo (optional (buff 34)))
+  )
   (begin
     (asserts! (is-eq tx-sender sender) (err u403))
-    (ft-transfer? my-token amount sender recipient)))
+    (ft-transfer? my-token amount sender recipient)
+  )
+)
 
 (define-read-only (get-name)
-  (ok "My Token"))
+  (ok "My Token")
+)
 
 (define-read-only (get-symbol)
-  (ok "MTK"))
+  (ok "MTK")
+)
 
 (define-read-only (get-decimals)
-  (ok u6))
+  (ok u6)
+)
 
 (define-read-only (get-balance (who principal))
-  (ok (ft-get-balance my-token who)))
+  (ok (ft-get-balance my-token who))
+)
 
 (define-read-only (get-total-supply)
-  (ok (ft-get-supply my-token)))
+  (ok (ft-get-supply my-token))
+)
 
 (define-read-only (get-token-uri)
-  (ok (var-get token-uri)))
+  (ok (var-get token-uri))
+)
 
 ;; Minting (owner only or simplified)
-(define-public (mint (amount uint) (recipient principal))
-  (ft-mint? my-token amount recipient))
+(define-public (mint
+    (amount uint)
+    (recipient principal)
+  )
+  (ft-mint? my-token amount recipient)
+)
---
+++ stxer-simulation-demo/index.ts
@@ -0,0 +1,58 @@
+/***
+ * You can access the simulation demo at https://stxer.xyz/simulations,
+ * along with a link to a pre-run simulation result: https://stxer.xyz/simulations/mainnet/00000000000000000000000000000101.
+ *
+ * This demo showcases the full capabilities of the simulation API.
+ * Feel free to fork and modify it to create your own custom version.
+ *
+ * The STXER SDK source code is available here: https://github.com/stxer/stxer-sdk
+ */
+
+import fs from 'node:fs';
+import { boolCV, ClarityVersion } from '@stacks/transactions';
+import { SimulationBuilder } from 'stxer';
+
+SimulationBuilder.new()
+  // or omit this to use tip height
+  .useBlockHeight(130818)
+  .addEvalCode(
+    'SP000000000000000000002Q6VF78.pox',
+    '(list block-height burn-block-height)'
+  )
+  // use this as the sender of the following steps
+  .withSender('SP1K1A1PMGW2ZJCNF46NWZWHG8TS1D23EGH1KNK60')
+  .addSTXTransfer({
+    recipient: 'SP212Y5JKN59YP3GYG07K3S8W5SSGE4KH6B5STXER',
+    amount: 10000,
+  })
+  .withSender('SP212Y5JKN59YP3GYG07K3S8W5SSGE4KH6B5STXER')
+  .addContractDeploy({
+    contract_name: 'test',
+    source_code: fs.readFileSync('demo.clar', 'utf8'),
+    // or omit this to use 0 fee, it's possible for simulation
+    fee: 100,
+    clarity_version: ClarityVersion.Clarity3,
+  })
+  .addContractCall({
+    contract_id: 'SP212Y5JKN59YP3GYG07K3S8W5SSGE4KH6B5STXER.test',
+    function_name: 'set-enabled',
+    function_args: [boolCV(true)],
+  })
+  .addVarRead('SP212Y5JKN59YP3GYG07K3S8W5SSGE4KH6B5STXER.test', 'enabled')
+  .addContractCall({
+    contract_id: 'SP212Y5JKN59YP3GYG07K3S8W5SSGE4KH6B5STXER.test',
+    function_name: 'set-enabled',
+    function_args: [boolCV(false)],
+  })
+  // call readonly function
+  .addEvalCode(
+    'SP212Y5JKN59YP3GYG07K3S8W5SSGE4KH6B5STXER.test',
+    '(get-enabled)'
+  )
+  // run arbitrary code
+  .addEvalCode(
+    'SP212Y5JKN59YP3GYG07K3S8W5SSGE4KH6B5STXER.test',
+    `(stx-get-balance 'SP212Y5JKN59YP3GYG07K3S8W5SSGE4KH6B5STXER)`
+  )
+  .run()
+  .catch(console.error);
